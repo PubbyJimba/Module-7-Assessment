401fc443df4e583ecd1e0d6f0dca01a957f4d637
Isaac Sant <isaacjosephsant@gmail.com>
2022-10-17 00:15:18 -0600

    Added isPangram function. Working.

--- runtime.js
+++ runtime.js
@@ -41,6 +41,7 @@ function sumToZero(numbers) {
   return false;
 }
 
+// Takes a single word as a string. Returns true if there are no repeated letters in the word.
 function hasUniqueChars(word) {
   for (let i = 0; i < word.length; i++) {
     for (let j = 0; j < word.length; j++) {
@@ -55,8 +56,17 @@ function hasUniqueChars(word) {
   return true;
 }
 
+// Takes a sentence as a string. Returns true is sentence is a pangram.
 function isPangram(sentence) {
-    
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let lowercase = sentence.toLowerCase();
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (lowercase.indexOf(alphabet[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
 }
 
 const tinyArray = getSizedArray(10);
@@ -66,6 +76,7 @@ const largeArray = getSizedArray(10000);
 const extraLargeArray = getSizedArray(100000);
 const numberArray = [0, 1, 2, 3, -2];
 const word = 'moonday';
+const sentence = 'Mr. Jock, TV quiz PhD., bags few lynx';
 
 // How long does it take to double every number in a given
 // array?
@@ -90,12 +101,23 @@ perf.start();
 hasUniqueChars(word);
 let resultsUnique = perf.stop();
 
+// Execution time for isPangram function.
+perf.start();
+isPangram(sentence);
+let resultsPangram = perf.stop();
+
 console.log('Results for the doubler functions');
 console.log('insert', resultsInsert.preciseWords);
 console.log('append', resultsAppend.preciseWords);
 
-console.log('Results for sumToZero function with numberArray');
+console.log(`Results for sumToZero function with ${numberArray}`);
+console.log(sumToZero(numberArray));
 console.log(sumToZeroResult.preciseWords);
 
-console.log("Results for hasUniqueChars functions with 'moonday'");
+console.log(`Results for hasUniqueChars function with ${word}`);
+console.log(hasUniqueChars(word));
 console.log(resultsUnique.preciseWords);
+
+console.log(`Results for isPangram function with ${sentence}`);
+console.log(isPangram(sentence));
+console.log(resultsPangram.preciseWords);
